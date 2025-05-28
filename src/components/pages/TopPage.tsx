120b5313d854d0e933b77a5343e491afaf7d5b6b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-05-29 01:27:35 +0900

    トップページ2

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export const TopPage = () => {
     return (
-        <div>
-            <header>
-                <div className="flex justify-between">
-                    <p>スケジュール管理App</p>
+        <div className="relative">
+            <header className="leading-[50px] fixed top-0 right-0 left-0">
+                <div className="container mx-auto flex justify-between">
+                    <p className="logo">スケジュール管理App</p>
                     <nav>
-                        <ul className="flex gap-5">
+                        <ul className="flex gap-5 text-lime-800">
                             <li>利用説明</li>
                             <li>ログイン</li>
                         </ul>
