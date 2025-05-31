2a3a2a737937c31a81a67671dd10e376258c651e
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-05-31 20:05:08 +0900

    ログイン画面作成

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,5 +1,29 @@
+import { PrimaryBtn } from '../atoms/PrimaryBtn'
+
+
 export const LoginPage = () => {
     return (
-        <div>ログイン</div>
+        <div className="w-[500px] bg-white rounded-lg shadow-lg py-10">
+            <form className="flex flex-col items-center justify-center gap-10">
+                <h1 className="text-3xl font-bold text-lime-800">
+                    ログイン
+                </h1>
+                <div className="w-[80%]">
+                    <input
+                        type="text"
+                        className="w-full border-4 border-solid border-lime-800 rounded-md p-2"
+                        placeholder="メールアドレス"
+                    />
+                </div>
+                <div className="w-[80%]">
+                    <input
+                        type="password"
+                        className="w-full border-4 border-solid border-lime-800 rounded-md p-2"
+                        placeholder="パスワード"
+                    />
+                </div>
+                <PrimaryBtn>ログイン</PrimaryBtn>
+            </form>
+        </div>
     )
 }
