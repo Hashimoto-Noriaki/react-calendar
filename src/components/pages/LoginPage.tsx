2fe4694434c2483917f700ff3effe50d45cd1cc9
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-01 17:47:49 +0900

    stateの用意

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,7 +1,19 @@
+import { ChangeEvent,useState } from 'react'
 import { PrimaryBtn } from '../atoms/PrimaryBtn'
 import { Input } from '../atoms/Input'
+import { loginInfoType } from '../../types/loginInfoType'
 
 export const LoginPage = () => {
+    const [loginInfo,setLoginInfo] = useState<loginInfoType>{
+        email:'',
+        password:'',
+    }
+
+    const changeLoginInfo = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name,value } = event.target
+        setLoginInfo({...loginInfo,[name]: value})
+    }
+
     return (
         <div className="w-[500px] bg-white rounded-lg shadow-lg py-10">
             <form className="flex flex-col items-center justify-center gap-10">
@@ -10,14 +22,20 @@ export const LoginPage = () => {
                 </h1>
                 <div className="w-[80%]">
                     <Input
+                        name="email"
                         type="text"
                         placeholder="メールアドレス"
+                        value={loginInfo.email}
+                        onChange={changeLoginInfo}
                     />
                 </div>
                 <div className="w-[80%]">
                     <Input
+                        name="password"
                         type="password"
                         placeholder="パスワード"
+                        value={loginInfo.password}
+                        onnchange={changeLoginInfo}
                     />
                 </div>
                 <PrimaryBtn onClick={()=> null}>ログイン</PrimaryBtn>
