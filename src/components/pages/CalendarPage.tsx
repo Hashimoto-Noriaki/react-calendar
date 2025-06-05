2ec9536a353b77f7e744908f96ed23897552144e
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-06 00:59:52 +0900

    カレンダーのテーブル表示(曜日と月)

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -1,13 +1,25 @@
-import { useLoginUser } from '../../hooks/useLoginUser'
+import { getMonth } from "date-fns";
+import { DAYS_LIST } from "../../constants/calendar";
 
 export const CalendarPage = () => {
-  const { loginUser } = useLoginUser();
-  return (
-    <div>
-      <p>{loginUser.id}</p>
-      <p>{loginUser.name}</p>
-    </div>
-  )
-}
+  const today = new Date()
 
-export default CalendarPage
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <h1 className="w-full text-center font-bold text-3xl mb-5">{`${getMonth(today) + 1}月`}</h1>
+        <table className="w-[80%] border-collapse border-2 border-solid border-lime-800 table-fixed">
+          <thead>
+            <tr className="bg-lime-800 text-white rounded-tl-lg rounded-tr-lg py-10">
+              {DAYS_LIST.map((day) => (
+                <th key={day} className="text-center text-xl py-3">
+                  {day}
+                </th>
+              ))}
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </>
+  );
+}
