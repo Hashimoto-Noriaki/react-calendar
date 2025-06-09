a13afb390ea9339f64e8f221a9f8bfc41835327b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-10 01:29:21 +0900

    リンクを追加

--- src/components/atoms/PrimaryBtn.tsx
+++ src/components/atoms/PrimaryBtn.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from 'react';
 
 type PropsType = {
+    onClick:() => void;
     children: ReactNode;
 }
 
-export const PrimaryBtn = ({children}: PropsType) => {
+export const PrimaryBtn = ({onClick,children}: PropsType) => {
     return (
-        <button className="p-5 bg-lime-800 text-white rounded-lg">
+        <button className="p-5 bg-lime-800 text-white text-lg rounded-lg" onClick={onClick}>
             {children}
         </button>
     )
