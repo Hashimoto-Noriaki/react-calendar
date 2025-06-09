53e72cc5882ef14b41011a28d9298c4add53f1e5
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-10 01:45:09 +0900

    ログイン機能(エラーメッセージ)

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,21 +1,53 @@
+import { ChangeEvent,FormEvent,useState } from "react";
 import { PrimaryBtn } from "../atoms/PrimaryBtn";
 import { Input } from "../atoms/Input";
 
 export const LoginPage = () => {
+    const [loginInfo, setLoginInfo] = useState<LoginInfoType>({
+        email: "",
+        password: ""
+    })
+
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const changeLoginInfo = (e: ChangeEvent<HTMLInputElement>)=> {
+        const { name,value } = e.target
+        setLoginInfo({...loginInfo,[name]: value})
+    }
+
+    const handleLogin = (e: FormEvent<HTMLFormElement>)=> {
+        e.preventDefault();
+        setErrorMessage("");
+        try {
+            login(setLoginInfo);
+        } catch {
+            setErrorMessage("ログインに失敗しました。");
+        }
+    }
+
     return (
         <div className="w-[500px] bg-white py-10 rounded-lg shadow-lg">
-            <form className="flex flex-col justify-center items-center gap-10">
+            <form className="flex flex-col justify-center items-center gap-10" onSubmit={handleLogin}>
+                {errorMessage !== '' && (
+                    <div className="bg-red-500 text-white rounded-lg p-5 w-[80%]">
+                        {errorMessage}
+                    </div>
+                )}
                 <h1 className="text-3xl text-center text-lime-800 font-bold">ログイン</h1>
                 <div className="w-[80%]">
                     <Input
                         name="email"
                         placeholder="メールアドレス"
+                        value={loginInfo.email}
+                        onChange={changeLoginInfo}
                     />
                 </div>
                 <div className="w-[80%]">
                     <Input 
                         password="password"
                         placeholder="パスワード"
+                        value={loginInfo.password}
+                        onChange={changeLoginInfo}
                     />
                 </div>
                 <PrimaryBtn onClick={()=> null}>ログイン</PrimaryBtn>
